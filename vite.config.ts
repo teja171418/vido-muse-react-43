bc79d78f59fd51f2c3f770c93c50bfbbc43a7b5c
teja171418 <teja171418@gmail.com>
2025-09-18 12:19:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port:'r4lt9m-8080.csb.app',
   },
   plugins: [
     react(),
